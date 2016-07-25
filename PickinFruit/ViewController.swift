080605c69a3bf25c6dcea7d5b8308fbe6a7de680
kenpcjr <kennethcooke@me.com>
2016-07-25 10:51:59 -0400

    all tests pass

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,22 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var fruitPicker: UIPickerView!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        fruitPicker.delegate = self
+        fruitPicker.dataSource = self
         
-        // self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
+        
+        
+        self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
         
     }
     
     @IBAction func spinButtonTapped(sender: UIButton) {
+        
+        
+        self.resultLabel.hidden = true
+        
+        let randomNumber0 = arc4random_uniform(UInt32(fruitsArray.count))
+        
+        self.fruitPicker.selectRow(Int(randomNumber0), inComponent: 0, animated: true)
+        
+        let resultingFruit0 = fruitsArray[self.fruitPicker.selectedRowInComponent(0)]
+        
+        
+        let randomNumber1 = arc4random_uniform(UInt32(fruitsArray.count))
+        
+        self.fruitPicker.selectRow(Int(randomNumber1), inComponent: 1, animated: true)
+        
+        let resultingFruit1 = fruitsArray[self.fruitPicker.selectedRowInComponent(1)]
+        
+        
+        let randomNumber2 = arc4random_uniform(UInt32(fruitsArray.count))
+        
+        self.fruitPicker.selectRow(Int(randomNumber2), inComponent: 2, animated: true)
+        
+        let resultingFruit2 = fruitsArray[self.fruitPicker.selectedRowInComponent(2)]
+        
+        checkForWin(resultingFruit0, result2: resultingFruit1, result3: resultingFruit2)
+        
+        
+        self.resultLabel.hidden = false
+        
+    }
+    
+    func checkForWin(result: String, result2: String, result3: String) {
+        
+        if result == result2 && result2 == result3 {
+            
+            self.resultLabel.text = "WINNER!"
+            
+        } else {
+            
+            self.resultLabel.text = "TRY AGAIN"
+        }
+        
         
         
     }
@@ -38,6 +86,20 @@ extension ViewController {
             configureButton()
         }
     }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return fruitsArray[row]
+    }
+    
     
     func configureButton()
     {
